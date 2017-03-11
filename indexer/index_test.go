5c41815b7d7b1dda066c0091b9d59707d29cef11
danny <dtylman@gmail.com>
2017-03-12 00:17:59 +0200

    fixing unit tests

--- indexer/index_test.go
+++ indexer/index_test.go
@@ -1,7 +1,6 @@
 package indexer
 
 import (
-	"github.com/dtylman/pictures/conf"
 	"github.com/dtylman/pictures/indexer/db"
 	"github.com/stretchr/testify/assert"
 	"testing"
@@ -9,11 +8,12 @@ import (
 )
 
 func TestStart(t *testing.T) {
-	conf.Options.SourceFolders = []string{"/home/danny/Pictures"}
+	err := db.Open()
+	if err != nil {
+		t.Fatal(err)
+	}
 	//geocoder.SetAPIKey("8cCGEGGioKhpCLPjhAG44NfXYaXs9jCk")
-	conf.Options.MapQuestAPIKey = "8cCGEGGioKhpCLPjhAG44NfXYaXs9jCk"
-	t.Log(conf.Options.SourceFolders)
-	err := Start(Options{IndexLocation: true, ReIndex: false})
+	err = Start(Options{IndexLocation: false, ReIndex: false})
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -24,13 +24,5 @@ func TestStart(t *testing.T) {
 	for IsRunning() {
 		time.Sleep(time.Second)
 		t.Log("Still running")
-	}
-	sr, err := db.QueryAll(0, 100)
-	if err != nil {
-		t.Fatal(err)
-	}
-	t.Log(sr.String())
-	for _, facet := range sr.Facets {
-		t.Log(facet)
 	}
 }
